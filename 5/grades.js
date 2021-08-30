a6af94c532ac8fcbe22304489a270490fd4f62cc
AVIV POLAK <avivp0437@gmail.com>
2021-08-30 20:00:40 +0300

    grades mission 2nd func
    +median func

--- 5/grades.js
+++ 5/grades.js
@@ -4,6 +4,16 @@ function range(grades){
     grades=grades.sort()   
     return grades[grades.length-1]-grades[0]
 }
+function median(grades){
+    grades=grades.sort()  
+    if(grades.length%2==0){
+        console.log(grades[grades.length/2])
+        return grades[grades.length/2]
+    }
+    console.log(grades[grades.length/2+0.5])
+    return grades[grades.length/2+0.5]
+}
 
+median(grades2)
 // your code goes here
 // console.log(...)
