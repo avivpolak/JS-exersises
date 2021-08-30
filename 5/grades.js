979e65ff1c2b0a453fca15beee1eec9b7d2e9394
AVIV POLAK <avivp0437@gmail.com>
2021-08-30 21:17:11 +0300

    grades mission display func
    + display func.
    debugging.
    intending.

--- 5/grades.js
+++ 5/grades.js
@@ -1,24 +1,35 @@
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
 const grades2= [73, 91, 88, 56, 62, 84, 95, 96, 88, 92, 100]
+const grades3= [73, 91, 88, 56, 62, 84, 95, 96, 88, 92, 100,88, 56, 62, 84, 95, 96, 88, 92]
 function range(grades){
-    grades=grades.sort()   
-    return grades[grades.length-1]-grades[0]
+    let studentsGrades = grades.slice();
+    studentsGrades= studentsGrades.sort(function(a, b){return a-b});
+    return studentsGrades[studentsGrades.length-1]-studentsGrades[0];
 }
 function median(grades){
-    grades=grades.sort()  
-    if(grades.length%2==0){
-        return grades[grades.length/2]
+    let studentsGrades = grades.slice();
+    studentsGrades= studentsGrades.sort(function(a, b){return a-b});  
+    if(studentsGrades.length%2==0){
+        return studentsGrades[studentsGrades.length/2];
     }
-    return grades[grades.length/2+0.5]
+    return studentsGrades[studentsGrades.length/2-0.5];//in case of odd num length of array, the median is at the end of the smaller half.
 }
 function halfRange(grades){
-    if(grades.length%2==0){
-        grades=grades.splice(0,grades.length/2)
+    let studentsGrades = grades.slice();
+    if(studentsGrades.length%2==0){
+        studentsGrades=studentsGrades.slice(studentsGrades.length/2,studentsGrades.length);
     }
     else{
-        grades=grades.splice(0,grades.length/2+0.5)
-    }
-    return range(grades)
+        studentsGrades=studentsGrades.slice(studentsGrades.length/2-0.5,studentsGrades.length);//in case of odd num length of array, second half of the year contains the bigger half. 
+    } 
+    return range(studentsGrades);
 }
-
-// console.log(...)
+function display(grades){
+console.log(
+    ":Stats"+"\n\t"+
+        "Range: "+range(grades)+"\n\t"+
+        "Median:" +median(grades)+"\n\t"+
+        "Half Range: "+halfRange(grades));}
+display(grades);
+display(grades2);
+display(grades3);
